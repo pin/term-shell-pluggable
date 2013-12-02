a464d20a5508c475d052bb5f5e2a39f067887783
Dmitri Popov <operator@cv.dp-net.com>
2013-12-02 00:14:42 -0500

    version bump

--- lib/Term/Shell/Pluggable.pm
+++ lib/Term/Shell/Pluggable.pm
@@ -13,11 +13,11 @@ Term::Shell::Pluggable - Pluggable command-line framework
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
